db0ac48af4e03328e8d3b756116e7cabad60e80d
Professor <135185597+professor-12@users.noreply.github.com>
2024-07-06 16:21:05 +0100

    Update carousel.tsx

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -4,11 +4,9 @@ import Card from "./UI/Card";
 const Carousel = () => {
     return (
         <div className="grid overflow-hidden grid-flow-col gap-[30px]">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+           
+          
+           
         </div>
     );
 };
