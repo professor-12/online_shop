aab1133c3d842c026a6d0407a6f7d9d3f877b7b0
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-07-09 21:59:27 +0100

    ssd

--- src/lib/dummy_data.ts
+++ src/lib/dummy_data.ts
@@ -18,17 +18,17 @@ export const dummy_data = [
 ] as const
 
 export const bestDealData = [
-     { name: "Fashion Sneakers 11", quantity: 20, price: "40,000" ,path:"/unsplash_EpmJBf-kQRE.png",fake:"240,000"},
-     { name: "Encap Sneakers N", quantity: 7, price: "350,000" , path:"/unsplash_fUEP0djb1hA.png",fake:"20,000"},
-     { name: "Fila Sneakers white", quantity: 3, price: "500,000" ,path:"/unsplash_SzVXG6FFh0Q.png",fake:"10,000"},
+      { name: "k-swiss Sneakers B", quantity: 40, price: "50,000" ,path:"/unsplash_jLEGurepDco.png",fake:"90,000"},
+     { name: "Encap Sneakers N", quantity: 7, price: "70,000" , path:"/unsplash_fUEP0djb1hA.png",fake:"100,000"},
+      { name: "k-swiss Heels 11", quantity: 10, price: "20,000", path: "/unsplash_Zx76sbAndc0.png", fake: "40,000" },
+      { name: "Nike Sneakers 11", quantity: 5, price: "200,000", path: "/unsplash_s-gYAbQToXk.png", fake: "290,000" },
+      
+       { name: "Puma Train Sneakers", quantity: 5, price: "150,000" , path:"/unsplash_HcqA34-uWo4.png",fake: "200,000"},
+
+
+     {  name: "Puma Train Sneakers", quantity: 5, price: "150,000",path:"/unsplash_t1sTHYjSN10.png",fake:"200,000"},
      { name: "Encap Sneakers N", quantity: 7, price: "350,000" , path:"/unsplash_fUEP0djb1hA.png",fake:"3,000"},
      { name: "Fashion Sneakers 11", quantity: 20, price: "40,000" ,path:"/unsplash_EpmJBf-kQRE.png",fake:"240,000"},
      { name: "k-swiss Sneakers B", quantity: 5, price: "40,000", path: "/unsplash_jLEGurepDco.png", fake: "70,000" }, 
-     { name: "Fashion Sneakers 11", quantity: 20, price: "40,000" ,path:"/unsplash_EpmJBf-kQRE.png",fake:"6,000"},
-     { name: "Fashion Sneakers 11", quantity: 20, price: "40,000" ,path:"/unsplash_EpmJBf-kQRE.png",fake:"7,00"},
-     { name: "k-swiss Sneakers B", quantity: 5, price: "40,000" ,path:"/unsplash_jLEGurepDco.png",fake:"4,00"}, 
-     { name: "k-swiss Sneakers B", quantity: 5, price: "40,000" ,path:"/unsplash_jLEGurepDco.png",fake:"90,00"}, 
-      
-      { name: "k-swiss Sneakers B", quantity: 5, price: "40,000", path: "/unsplash_jLEGurepDco.png", fake: "70,000" }, 
-      { name: "Fashion Sneakers 11", quantity: 20, price: "40,000" ,path:"/unsplash_EpmJBf-kQRE.png",fake:"240,000"},
+         
 ] as const
