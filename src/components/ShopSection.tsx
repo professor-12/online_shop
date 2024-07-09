b1e7c1330ccefb30a7d16902b5818c71058920bb
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-07-09 21:24:25 +0100

    mend

--- src/components/ShopSection.tsx
+++ src/components/ShopSection.tsx
@@ -9,6 +9,17 @@ const ShopSection = () => {
     const [hidden, setHidden] = useState(true);
     return (
         <section id="shop" className="space-y-[51px] px-2">
+            <div className="space-y-[38px]">
+                <div className="space-y-[10px]">
+                    <h2 className="text-[20px] md:text-[36px] text-center font-semibold">
+                        Shop Now, Turn Heads Later
+                    </h2>
+                    <p className="text-gray md:leading-[24px] text-center text-[16px] md:text-[20px]">
+                        Don’t wait to upgrade your wardrobe! Grab your favorites
+                        now and be <br /> ready to turn heads wherever you go.{" "}
+                    </p>
+                </div>
+            </div>
             <div className="xl:w-[80%]  px-3 max-md:justify-between justify-end max-md:flex-row-reverse xl:px-0 lg:px-6 mx-auto flex">
                 <button
                     onClick={() => setHidden((prev) => !prev)}
@@ -22,17 +33,6 @@ const ShopSection = () => {
                         height={20}
                     />
                 </button>
-            </div>
-            <div className="space-y-[38px]">
-                <div className="space-y-[10px]">
-                    <h2 className="text-[20px] md:text-[36px] text-center font-semibold">
-                        Shop Now, Turn Heads Later
-                    </h2>
-                    <p className="text-gray md:leading-[24px] text-center text-[16px] md:text-[20px]">
-                        Don’t wait to upgrade your wardrobe! Grab your favorites
-                        now and be <br /> ready to turn heads wherever you go.{" "}
-                    </p>
-                </div>
             </div>
             <div className="relative">
                 <Products hidden={hidden} setHidden={setHidden} />
