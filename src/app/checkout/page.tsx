4f20143a1c767bfe6690d6b0d11a7d320f1a7a4e
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-07-09 09:06:24 +0100

    s

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -19,11 +19,13 @@ const page = () => {
                 <div>$50,000</div>
             </div>
             <div className="mx-auto w-full md:w-[80%] px-2 lg:w-full  xl:w-[80%] lg:px-5    max-lg:flex-col max-lg:gap-[24px]  py-12 flex">
-                <div className="lg:flex-1 px-5 lg:px-[68px] bg-secondary-darkBlue max-md:hidden  py-[30px] rounded-[30px]">
-                    <OrderSummary />
-                </div>
                 <div className="lg:flex-1 mx-auto  bg-[#ECEFFF] border-[#9DAEEA]  border rounded-[20px] py-[30px] px-5 md:px-[68px]">
                     <Payment />
+                </div>
+                <div>
+                    <div className="lg:flex-1 px-5 lg:px-[68px] bg-secondary-darkBlue max-md:hidden  py-[30px] rounded-[30px]">
+                        <OrderSummary />
+                    </div>
                 </div>
             </div>
         </div>
